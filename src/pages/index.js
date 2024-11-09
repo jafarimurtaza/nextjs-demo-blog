3003c64ad6a4dc3b5027a1d6c07e6b7be10abf36
Murtaza Jafari <65913750+jafarimurtaza@users.noreply.github.com>
2024-11-09 19:10:57 +0100

    Created a paragrap tag in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,9 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <h1>Hello and welcome to the Afghan Geeks platform.</h1>
+      <p>
+        This is a great place to learn and share knowledge about technology.
+      </p>
     </div>
   );
 }
